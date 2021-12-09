7172aba7a8626561e3471fc232410c4941a7015c
Brunstart <Brunstart@gmail.com>
2021-12-10 01:11:30 +0900

    211209 adapter pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,13 +1,16 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-    public static void main(String[] args) {
-
+/*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -16,5 +19,22 @@ public class Main {
 
         System.out.println("두개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+*/
+
+        HairDrier hairDrier = new HairDrier();
+        connect(hairDrier);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+
+    public static void connect(Electronic110V electronic110V)
+    {
+        electronic110V.powerOn();
     }
 }
